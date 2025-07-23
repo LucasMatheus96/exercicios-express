2baa7c4079ee9cd4ad57d709357f39dfed35e0da
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-07-22 22:14:32 -0300

    Express_Middleware_01

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000 , ()=>{
-    console.log('backendExecutando')
+
+
+app.get('/opa',(req,res)=>{
+
+    res.json([
+        {id: 7, name:'ana', position:1},
+        {id: 34, name:'Bia', position:2},
+        {id: 73, name:'Carlos', position:3},
+    ])
+    // res.json({
+    //     name:'ipad 32gb',
+    //     price:1899.00,
+    //     discount:0.12
+    // })
+    // res.send("/opa", "estou bem");
 })
+
+app.use('/opa',(req,res)=>{
+    console.log('Será que serei chamado')
+})
+
+app.listen(3001, ()=>{ 
+})
+
